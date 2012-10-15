dafda187e0a98f8c50074f60ec0428724e0cb077
Ahmad Sherif <ahmad.sherif@mashsolvents.com>
2012-10-15 13:27:38 +0200

    Actual Raneen OmniAuth strategy code

---
+++ lib/omniauth/strategies/raneen.rb
@@ -0,0 +1,35 @@
+require 'omniauth/strategies/oauth2'
+
+module OmniAuth
+  module Strategies
+    class Raneen < OmniAuth::Strategies::OAuth2
+      option :name, 'raneen'
+      option :provider_ignores_state, true
+
+      option :client_options, {
+        site: 'http://localhost:3000',
+        token_url: '/oauth/access_token',
+        token_method: :get,
+      }
+
+      uid{ raw_info['id'] }
+
+      info do
+        {
+          :name => [raw_info['first_name'], raw_info['last_name']].join(' '),
+          :email => raw_info['email']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v1/users/index').parsed
+      end
+    end
+  end
+end
