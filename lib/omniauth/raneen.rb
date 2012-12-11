e9624258fcf9926644d6a438248090d76ff04581
ranaramez <rana.ramez@gmail.com>
2012-12-11 13:07:48 +0200

    added require for the generator

--- lib/omniauth/raneen.rb
+++ lib/omniauth/raneen.rb
@@ -1,10 +1,8 @@
 require_relative 'raneen/version'
 require "rails"
-# require_relative 'raneen/railtie'
 require_relative 'raneen/configuration'
-
-# require_relative 'raneen/configuration'
 require_relative 'raneen/railtie'
+require_relative '../generators/omniauth-raneen/install_generator'
 
 module Omniauth
   module Raneen
